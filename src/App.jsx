e3662f7385b97faa4d27ebb42ddd2759f0f8c1fe
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-06-23 23:51:51 -0400

    wip

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import HelloWorld from './components/HelloWorld';
+
+const App = () => {
+  return (
+    <HelloWorld />
+  )
+}
+
+export default App;
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+const HelloWorld = () => {
+  return (
+    <h1>Caio super lindo</h1>
+  )
+}
+
+export default HelloWorld;
